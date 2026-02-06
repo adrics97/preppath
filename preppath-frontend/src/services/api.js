824efaed92bcf5fa84c237b3297b7b7feb14a55b
Adrian Sanchis <adrics97@gmail.com>
2026-02-07 00:04:19 +0100

    Update API URL to EC2

--- preppath-frontend/src/services/api.js
+++ preppath-frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+
+const API_URL = 'http://34.235.129.159:8080/api';
+//const API_URL_DEV = 'http://localhost:8080/api';
 
 const api = axios.create({
     baseURL: API_URL,
