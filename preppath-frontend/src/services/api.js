e6711437e9f7eed17581ebffc75c2914bb093658
Adrian Sanchis <adrics97@gmail.com>
2026-02-07 01:07:33 +0100

    Update API URL to HTTPS

--- preppath-frontend/src/services/api.js
+++ preppath-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://34.235.129.159:8080/api';
+const API_URL = 'https://api.preppathapp.com/api';
 //const API_URL_DEV = 'http://localhost:8080/api';
 
 const api = axios.create({
