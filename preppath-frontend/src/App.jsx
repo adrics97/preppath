8e29f9dcdc706c69fca0965619b81f0245955fc8
Adrian Sanchis <adrics97@gmail.com>
2026-02-09 12:23:44 +0100

    fix: import helmet provider

--- preppath-frontend/src/App.jsx
+++ preppath-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { useAuth } from './context/useAuth';
+import { HelmetProvider } from 'react-helmet-async';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
